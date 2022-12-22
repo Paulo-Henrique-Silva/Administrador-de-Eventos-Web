adefb531df023e7be6f591e987028415e6d62f9e
Paulo <pshenrique2015@gmail.com>
2022-12-22 17:40:15 -0300

    Layout da página de entrar.

---
+++ EventzManager/Pages/Login/Cadastro.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EventzManager.Pages.Login
+{
+    public class CadastroModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ EventzManager/Pages/Login/EsqueceuSenha.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EventzManager.Pages.Login
+{
+    public class EsqueceuSenhaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
