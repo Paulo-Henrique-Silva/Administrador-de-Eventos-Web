220ca9d9b95967c19a203724145a7715711ad2d7
Paulo <pshenrique2015@gmail.com>
2022-12-27 12:44:46 -0300

    Implantação básica da funcionalidade de cadastro.

--- EventzManager/Pages/Login/Cadastro/ConfirmarEmail.cshtml.cs
+++ EventzManager/Pages/Login/Cadastro/ConfirmarEmail.cshtml.cs
@@ -1,12 +1,33 @@
+using EventzManager.Modelos;
+using EventzManager.Pages.Login.EsqueceuSenha;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace EventzManager.Pages.Login.Cadastro
 {
     public class ConfirmarEmailModel : PageModel
     {
-        public void OnGet()
+        public Usuario NovoUsario { get; set; } = new Usuario();
+
+        public readonly string Codigo = $"{new Random().Next(100):D2}{new Random().Next(100):D2}{new Random().Next(100):D2}";
+
+        public void OnGet(Usuario novoUsuario)
         {
+            NovoUsario = novoUsuario;
+        }
+
+        public IActionResult OnPostConfirmar(string CodigoInserido)
+        {
+            if (CodigoInserido != Codigo)
+            {
+                ModelState.AddModelError("Codigo", "Código incorreto.");
+            }
+
+            if (ModelState.IsValid)
+                return RedirectToPage("/Principal/ListaEventos");
+
+            return Page();
         }
     }
 }
--- EventzManager/Pages/Login/Cadastro/PreecherDados.cshtml.cs
+++ EventzManager/Pages/Login/Cadastro/PreecherDados.cshtml.cs
@@ -1,17 +1,26 @@
+using EventzManager.Modelos;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace EventzManager.Pages.Login.Cadastro
 {
     public class PreecherDadosModel : PageModel
     {
+        [BindProperty]
+        public Usuario NovoUsario { get; set; } = new Usuario();
+
         public void OnGet()
         {
+
         }
 
         public IActionResult OnPostCadastrar()
         {
-            return RedirectToPage("/Login/Cadastro/ConfirmarEmail");
+            if (ModelState.IsValid)
+                return RedirectToPage("/Login/Cadastro/ConfirmarEmail", NovoUsario);
+
+            return Page(); //atualiza a página novamente caso a validação não se suceda
         }
     }
 }
