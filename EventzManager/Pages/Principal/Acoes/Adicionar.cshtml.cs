4ae684aca3a66d3e92b503f62f3050eb28ed3e21
Paulo <pshenrique2015@gmail.com>
2022-12-28 18:00:26 -0300

    Implantação parcial da funcionalidade de adicionar eventos.

--- EventzManager/Pages/Principal/Acoes/Adicionar.cshtml.cs
+++ EventzManager/Pages/Principal/Acoes/Adicionar.cshtml.cs
@@ -1,3 +1,4 @@
+using EventzManager.Modelos;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,45 @@ namespace EventzManager.Pages.Principal.Acoes
 {
     public class AdicionarModel : PageModel
     {
-        public void OnGet()
+        public Evento NovoEvento { get; set; } = new Evento();
+
+        private readonly BancoDeDados Contexto;
+
+        public AdicionarModel(BancoDeDados contexto)
         {
+            Contexto = contexto;
+        }
+
+        public void OnGet(uint id)
+        {
+            Usuario? usuario = Contexto.Usuarios.Find(id);
+
+            if (usuario != null) //checa para caso a conta exista
+            {
+                TempData["primeiro_nome"] = usuario.Nome[..usuario.Nome.IndexOf(' ')]; //obtém o primeiro nome do usuário
+                TempData["id"] = id;
+                NovoEvento.Usuario = usuario;
+            }
+        }
+
+        public IActionResult OnPostAdicionar()
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    Contexto.Eventos.Add(NovoEvento);
+                    Contexto.SaveChanges();
+
+                    return RedirectToPage("/Principal/ListaEventos", new { Id = 34 });
+                }
+                catch (Exception ex)
+                {
+                    TempData["erro"] = ex.Message + "\nNão foi possível adicionar. Tente novamente.";
+                }
+            }
+
+            return Page();
         }
     }
 }
