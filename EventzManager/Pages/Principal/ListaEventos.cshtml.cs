5c3368a3f3ff4b707986752c4c92dad4780cdaa6
Paulo <pshenrique2015@gmail.com>
2023-01-03 11:52:23 -0300

    Lista de eventos na página principal.

--- EventzManager/Pages/Principal/ListaEventos.cshtml.cs
+++ EventzManager/Pages/Principal/ListaEventos.cshtml.cs
@@ -6,6 +6,8 @@ namespace EventzManager.Pages.Principal
 {
     public class ListaEventosModel : PageModel
     {
+        public List<Evento> Eventos { get; set; } = new List<Evento>();
+
         private readonly BancoDeDados Contexto;
 
         public ListaEventosModel(BancoDeDados contexto)
@@ -17,10 +19,24 @@ namespace EventzManager.Pages.Principal
         {
             Usuario? usuario = Contexto.Usuarios.Find(id);
 
-            if (usuario != null) //checa para caso a conta exista
+            if (usuario == null) //checa para caso a conta exista
+                return;
+
+            TempData["primeiro_nome"] = usuario.Nome[..usuario.Nome.IndexOf(' ')]; //obtém o primeiro nome do usuário
+            TempData["id"] = id;
+
+            //obtém lista de eventos
+            List<Evento> eventos = Contexto.Eventos.Where(e => e.UsuarioId == id).OrderBy(x => x.Data).ToList();
+            Eventos = eventos.ToList();
+
+            //coloca os eventos que possuem a data expirada por último na lista.
+            foreach (var evento in eventos)
             {
-                TempData["primeiro_nome"] = usuario.Nome[..usuario.Nome.IndexOf(' ')]; //obtém o primeiro nome do usuário
-                TempData["id"] = id;
+                if (evento.Data < DateTime.Now)
+                {
+                    Eventos.Remove(evento);
+                    Eventos.Add(evento);
+                }
             }
         }
     }
