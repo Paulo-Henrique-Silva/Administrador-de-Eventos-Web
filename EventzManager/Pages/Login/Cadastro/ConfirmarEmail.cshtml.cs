21ca66376366cf4fbded362651a9d04b7a65d450
Paulo <pshenrique2015@gmail.com>
2023-01-09 12:07:36 -0300

    Pequenas mudanças.

--- EventzManager/Pages/Login/Cadastro/ConfirmarEmail.cshtml.cs
+++ EventzManager/Pages/Login/Cadastro/ConfirmarEmail.cshtml.cs
@@ -35,7 +35,7 @@ namespace EventzManager.Pages.Login.Cadastro
                 var emailObjeto = new MimeMessage();
                 var smtp = new SmtpClient();
 
-                //insira o seu próprio email personalizado aqui
+                //insira o seu email personalizado aqui
                 string email = "";
                 string senha = "";
 
